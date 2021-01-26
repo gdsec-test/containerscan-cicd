3928e847226dff9a3d9e9ec11da554e00c6b7d0f
shawn <72945391+schoi-godaddy@users.noreply.github.com>
2021-01-26 09:08:23 -0800

    Add primsa result conversion & output logic (#1)

--- docker/container_scan.go
+++ docker/container_scan.go
@@ -8,17 +8,51 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/cenkalti/backoff/v4"
+	"github.com/olekukonko/tablewriter"
+)
+
+var (
+	colorReset  = "\033[0m"
+	colorRed    = "\033[31m"
+	colorGreen  = "\033[32m"
+	colorYellow = "\033[33m"
+	colorBlue   = "\033[34m"
+	colorPurple = "\033[35m"
+	colorCyan   = "\033[36m"
+	colorWhite  = "\033[37m"
+
+	CONSOLEURI = "https://us-east1.cloud.twistlock.com/us-2-158254964"
+
+	accesskey string
+	secretid  string
+	container string
 )
 
 type token struct {
 	Token string
 }
 
-func getAuthToken(CONSOLEURI string, username string, password string) token {
+type ScanResult struct {
+	ComplianceIssues
+	Vulnerabilities
+}
+
+type ComplianceIssues []map[string]interface{}
+type Vulnerabilities []map[string]interface{}
+
+func init() {
+	arg := os.Args
+	accesskey = arg[1]
+	secretid = arg[2]
+	container = arg[3]
+}
+
+func getAuthToken(username string, password string) token {
 
 	url := CONSOLEURI + "/api/v1/authenticate"
 
@@ -79,7 +113,7 @@ func getAPIResponse(url string, authToken string) []byte {
 	return body
 }
 
-func downloadTwistCli(CONSOLEURI string, token string) {
+func downloadTwistCli(token string) {
 	twistcli := getAPIResponse(CONSOLEURI+"/api/v1/util/twistcli", token)
 
 	err := ioutil.WriteFile("twistcli", twistcli, 0755)
@@ -89,49 +123,165 @@ func downloadTwistCli(CONSOLEURI string, token string) {
 	}
 }
 
-func runTwistCli(url string, token string, container string) {
-	colorRed := "\033[31m"
-	cmd := exec.Command("/bin/sh", "-c", "./twistcli images scan --details --address "+url+"  --token "+token+" --ci "+container)
+func runTwistCli(token string, container string) string {
+	cmd := exec.Command("/bin/sh", "-c", "./twistcli images scan --details --address "+CONSOLEURI+" --token "+token+" --ci "+container)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println(string(colorRed), fmt.Sprint(err)+": "+stderr.String())
-		return
+		printWithColor(colorRed, "Error : Unexpected error while executing twistlock cli", err, stderr.String())
+		os.Exit(1)
 	}
 
-	resultstring := out.String()
-	resultstring = resultstring[:strings.Index(resultstring, "=====DATA[{")]
+	return out.String()
+}
 
-	fmt.Println("Result: " + resultstring)
+func formatTwistlockResult(resultstring string) ScanResult {
+	delimiter := "====DATA"
+	res := resultstring[strings.Index(resultstring, delimiter)+len(delimiter):]
+
+	s := []struct {
+		EntityInfo ScanResult
+	}{}
+
+	if err := json.Unmarshal([]byte(res), &s); err != nil {
+		printWithColor(colorRed, "Error : Unable to unmarshal twistlock result", err)
+		os.Exit(1)
+	}
+
+	return s[0].EntityInfo
+}
+
+func (res *ScanResult) normalize() {
+	res.ComplianceIssues.normalize()
+	res.Vulnerabilities.normalize()
+	fmt.Println("")
+}
+
+func (comp *ComplianceIssues) normalize() {
+	printWithColor(colorBlue, "ComplianceIssues Normalization Not Implemented.")
+}
+
+func (vuln *Vulnerabilities) normalize() {
+	printWithColor(colorBlue, "Vulnerabilities Normalization Not Implemented.")
+}
+
+func (res ScanResult) reportToCLI() {
+	cFound := res.ComplianceIssues.reportToCLI()
+	fmt.Println()
+	vFound := res.Vulnerabilities.reportToCLI()
+
+	if cFound || vFound {
+		printWithColor(colorRed, "\nFAILED : Above issue(s) found with the Image.")
+		os.Exit(1)
+	} else {
+		printWithColor(colorGreen, "\nSUCCESS : Could not find any issue(s) with the Image.")
+	}
+}
+
+func (comp ComplianceIssues) reportToCLI() bool {
+	if len(comp) == 0 {
+		return false
+	}
+
+	printWithColor(colorRed, "Compliance Issues :")
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Id", "Severity", "Title", "Description", "Type"})
+	table.SetRowLine(true)
+	table.SetRowSeparator("-")
+	table.SetHeaderColor(tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold})
+
+	for _, c := range comp {
+		table.Append([]string{
+			strconv.FormatFloat(c["id"].(float64), 'f', 0, 64),
+			c["severity"].(string),
+			c["title"].(string),
+			c["description"].(string),
+			c["type"].(string),
+		})
+	}
+
+	table.Render()
+
+	return true
+}
+
+func (vuln Vulnerabilities) reportToCLI() bool {
+	if len(vuln) == 0 {
+		return false
+	}
+
+	printWithColor(colorRed, "Vulnerability Issues :")
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{
+		"CVE",
+		"CVSS",
+		"Severity",
+		"Status",
+		"Package Name",
+		"Package Version",
+		"Description",
+		"Type",
+		"Link",
+	})
+	table.SetRowLine(true)
+	table.SetRowSeparator("-")
+	table.SetHeaderColor(tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold})
+
+	for _, v := range vuln {
+		table.Append([]string{
+			v["cve"].(string),
+			strconv.FormatFloat(v["cvss"].(float64), 'f', 2, 64),
+			v["severity"].(string),
+			v["status"].(string),
+			v["packageName"].(string),
+			v["packageVersion"].(string),
+			v["description"].(string),
+			v["type"].(string),
+			"https://web.nvd.nist.gov/view/vuln/detail?vulnId=" + v["cve"].(string),
+		})
+	}
+
+	table.Render()
+
+	return true
+}
+
+func printWithColor(color string, str ...interface{}) {
+	fmt.Print(color)
+	for _, v := range str {
+		fmt.Println(v)
+	}
+	fmt.Print(colorReset)
 }
 
 func main() {
+	printWithColor(colorGreen, "Container Scanner Started\n")
 
-	arg := os.Args
-	// colorReset := "\033[0m"
+	token := getAuthToken(accesskey, secretid)
 
-	//colorRed := "\033[31m"
-	colorGreen := "\033[32m"
-	// colorYellow := "\033[33m"
-	// colorBlue := "\033[34m"
-	// colorPurple := "\033[35m"
-	// colorCyan := "\033[36m"
-	// colorWhite := "\033[37m"
+	downloadTwistCli(token.Token)
 
-	fmt.Println(string(colorGreen), "Run....")
+	resultstring := runTwistCli(token.Token, container)
+	scanResult := formatTwistlockResult(resultstring)
 
-	CONSOLEURI := "https://us-east1.cloud.twistlock.com/us-2-158254964"
-	accesskey := arg[1]
-	secretid := arg[2]
-	container := arg[3]
-	token := getAuthToken(CONSOLEURI, accesskey, secretid)
+	scanResult.normalize()
 
-	downloadTwistCli(CONSOLEURI, token.Token)
-
-	runTwistCli(CONSOLEURI, token.Token, container)
-	// none-zero status, fails step
-	// os.Exit(3)
+	scanResult.reportToCLI()
 }
--- docker/go.mod
+++ docker/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/cenkalti/backoff/v4 v4.1.0
 	github.com/go-ini/ini v1.62.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/olekukonko/tablewriter v0.0.4 // indirect
 	github.com/smartystreets/goconvey v1.6.4 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
